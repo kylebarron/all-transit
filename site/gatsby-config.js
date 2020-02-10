fb592bd9e3306659340ccf9edbc43d7d70ff0d14
Kyle Barron <barronk@mit.edu>
2020-02-10 18:12:07 -0500

    Turn off web manifest and gatsby-plugin-offline

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -64,23 +64,23 @@ module.exports = {
     // web manifest file so your website can be added to your
     // homescreen on Android.
     /* eslint-disable camelcase */
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby website`,
-        short_name: `Gatsby website`,
-        start_url: `/`,
-        background_color: `#f7f7f7`,
-        theme_color: `#191919`,
-        display: `minimal-ui`
-      }
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `Gatsby website`,
+    //     short_name: `Gatsby website`,
+    //     start_url: `/`,
+    //     background_color: `#f7f7f7`,
+    //     theme_color: `#191919`,
+    //     display: `minimal-ui`
+    //   }
+    // },
     /* eslint-enable camelcase */
 
     // This plugin generates a service worker and AppShell
     // html file so the site works offline and is otherwise
     // resistant to bad networks. Works with almost any
     // site!
-    `gatsby-plugin-offline`
+    // `gatsby-plugin-offline`
   ]
 };
