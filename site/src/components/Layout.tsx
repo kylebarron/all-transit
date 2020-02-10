bb88a93f6f739f7f085541716c4dfb7dfbd6d051
Kyle Barron <barronk@mit.edu>
2020-02-10 17:50:58 -0500

    Remove blog things

--- site/src/components/Layout.tsx
+++ site/src/components/Layout.tsx
@@ -13,7 +13,6 @@ import { store } from "../store";
 export const menuItems = [
   { name: "Home", path: "/", exact: true, icon: "home", inverted: true },
   { name: "About", path: "/about/", exact: true, icon: "info circle" },
-  { name: "Blog", path: "/blog/", exact: false, icon: "newspaper" },
 ];
 
 export interface LayoutProps {
