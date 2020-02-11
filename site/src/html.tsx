f8efa9017bfddf129ddef24e903b58e9b5a4bf3a
Kyle Barron <barronk@mit.edu>
2020-02-11 00:55:29 -0500

    Change site name to "All Transit"

--- site/src/html.tsx
+++ site/src/html.tsx
@@ -23,7 +23,7 @@ export default (props: HtmlProps) => {
     <html lang="en">
       <head>
         {props.headComponents}
-        <title>My website</title>
+        <title>All Transit</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
