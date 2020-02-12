0b61600b3022f310653c01c3a7cdc3338470ed05
Kyle Barron <barronk@mit.edu>
2020-02-12 15:20:30 -0500

    Cut geojson features into geojson tiles

---
+++ code/tile_geojson.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+
+import click
+import cligj
+import geojson
+import mercantile
+from shapely.geometry import asShape, box
+from shapely.ops import split
+
+
+@click.command()
+@cligj.features_in_arg
+@click.option(
+    '-z',
+    '--min-zoom',
+    type=int,
+    required=True,
+    help='Min zoom level to create tiles for',
+)
+@click.option(
+    '-Z',
+    '--max-zoom',
+    type=int,
+    required=True,
+    help='Max zoom level to create tiles for (inclusive)',
+)
+@click.option(
+    '-d',
+    '--tile-dir',
+    type=click.Path(file_okay=False, dir_okay=True, writable=True))
+def cut_geojson(features, min_zoom, max_zoom, tile_dir):
+    """Cut GeoJSON features into xyz tiles
+    """
+    tile_dir = Path(tile_dir)
+
+    for feature in features:
+        geometry = asShape(feature['geometry'])
+        tiles = find_tiles(geometry, min_zoom, max_zoom)
+
+        for tile in tiles:
+            new_feature = geojson.Feature(
+                geometry=clip_geometry_to_tile(geometry, tile),
+                properties=feature['properties'])
+
+            # Write feature to tile_dir
+            this_tile_dir = (tile_dir / str(tile.z) / str(tile.x))
+            this_tile_dir.mkdir(parents=True, exist_ok=True)
+            with open(this_tile_dir / f'{str(tile.y)}.geojson', 'a') as f:
+                f.write(geojson.dumps(new_feature, separators=(',', ':')))
+
+
+def find_tiles(geometry, min_zoom, max_zoom):
+    assert min_zoom <= max_zoom, 'min zoom must be <= max zoom'
+
+    selected_tiles = []
+
+    bound_tiles = mercantile.tiles(
+        *geometry.bounds, zooms=range(min_zoom, max_zoom + 1))
+    for tile in bound_tiles:
+        if box(*mercantile.bounds(tile)).intersects(geometry):
+            selected_tiles.append(tile)
+
+    return selected_tiles
+
+
+def clip_geometry_to_tile(geometry, tile):
+    tile_geom = box(*mercantile.bounds(tile))
+
+    # Geometry collection of split objects
+    split_gc = split(geometry, tile_geom)
+
+    return [g for g in split_gc if tile_geom.contains(g)]
+
+
+if __name__ == '__main__':
+    cut_geojson()
