e9bb5f05d7582e53ee03071f89d7a92c273319f6
Kyle Barron <barronk@mit.edu>
2020-02-12 16:24:28 -0500

    Allow skipping writing features by geometry type

--- code/tile_geojson.py
+++ code/tile_geojson.py
@@ -28,9 +28,21 @@ from shapely.ops import split
     '-d',
     '--tile-dir',
     type=click.Path(file_okay=False, dir_okay=True, writable=True))
-def cut_geojson(features, min_zoom, max_zoom, tile_dir):
+@click.option(
+    '--allowed-geom-type',
+    type=str,
+    required=False,
+    default=[],
+    help='Geometry types to keep in exported GeoJSON features.')
+def cut_geojson(features, min_zoom, max_zoom, tile_dir, allowed_geom_type):
     """Cut GeoJSON features into xyz tiles
     """
+    geometry_types = [
+        'Point', 'MultiPoint', 'LineString', 'MultiLineString', 'Polygon',
+        'MultiPolygon']
+    if not all(t in geometry_types for t in allowed_geom_type):
+        raise ValueError(f'allowed_geom_type must be one of: {geometry_types}')
+
     tile_dir = Path(tile_dir)
 
     for feature in features:
@@ -40,9 +52,18 @@ def cut_geojson(features, min_zoom, max_zoom, tile_dir):
         for tile in tiles:
             clipped_geometries = clip_geometry_to_tile(geometry, tile)
 
-            new_features = [
-                geojson.Feature(geometry=c, properties=feature['properties'])
-                for c in clipped_geometries]
+            new_features = []
+            for clipped_geometry in clipped_geometries:
+                if allowed_geom_type:
+                    geom_type = clipped_geometry.type
+                    if geom_type not in allowed_geom_type:
+                        print(f'Skipping feature of type: {geom_type}')
+                        continue
+
+                new_features.append(
+                    geojson.Feature(
+                        geometry=clipped_geometry,
+                        properties=feature['properties']))
 
             # Write feature to tile_dir
             this_tile_dir = (tile_dir / str(tile.z) / str(tile.x))
