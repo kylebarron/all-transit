e6abb218abcae3abfef78ce5d0cbcb3da6c4efd7
Kyle Barron <barronk@mit.edu>
2020-02-14 15:55:44 -0500

    Remove unused imports

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,32 +1,7 @@
 import * as React from "react";
-import { Link } from "gatsby";
-import HeaderMenu from "../components/HeaderMenu/HeaderMenu";
-import { withLayout, LayoutProps, menuItems } from "../components/Layout";
-import {
-  Button,
-  Segment,
-  Container,
-  Grid,
-  Header,
-  Icon,
-} from "semantic-ui-react";
-
+import { LayoutProps } from "../components/Layout";
 import Map from "../components/Map";
 
-const IndexPage = (props: LayoutProps) => (
-  <div>
-    <Map location={props.location} />
-
-    {/* Master head */}
-    {/* <Segment vertical inverted textAlign="center" className="masthead">
-      <HeaderMenu
-        Link={Link}
-        pathname={props.location.pathname}
-        items={menuItems}
-        inverted
-      />
-    </Segment> */}
-  </div>
-);
+const IndexPage = (props: LayoutProps) => <Map location={props.location} />;
 
 export default IndexPage;
